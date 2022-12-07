5acec687af3641a487aabd602e92a68a35487543
Cuma Özavcı <cumaozavci@outlook.com>
2022-12-07 08:09:44 +0300

    Change QoS profile

--- src/AprilTagNode.cpp
+++ src/AprilTagNode.cpp
@@ -134,7 +134,7 @@ AprilTagNode::AprilTagNode(const rclcpp::NodeOptions &options)
       cb_parameter(add_on_set_parameters_callback(std::bind(&AprilTagNode::onParameter, this, std::placeholders::_1))),
       td(apriltag_detector_create()),
       // topics
-      sub_cam(image_transport::create_camera_subscription(this, "image_rect", std::bind(&AprilTagNode::onCamera, this, std::placeholders::_1, std::placeholders::_2), declare_parameter("image_transport", "raw", descr({}, true)), rmw_qos_profile_sensor_data)),
+      sub_cam(image_transport::create_camera_subscription(this, "image_rect", std::bind(&AprilTagNode::onCamera, this, std::placeholders::_1, std::placeholders::_2), declare_parameter("image_transport", "raw", descr({}, true)), rmw_qos_profile_default)),
       pub_detections(create_publisher<apriltag_msgs::msg::AprilTagDetectionArray>("detections", rclcpp::QoS(1))),
       tf_broadcaster(this)
 {
